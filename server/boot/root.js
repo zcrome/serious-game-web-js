645ba50522f0e70dfc4f08df51111c639a1baa8f
zcrome <soportezcrome@gmail.com>
2016-09-12 00:21:25 -0500

    agregacion de la pagina index. moficacion del middleware

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,11 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+
+
+
+
+
+//END
   server.use(router);
 };
